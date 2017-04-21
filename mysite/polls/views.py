f3fd2cd6ada0a647b9d9a1d6a1a5c0fe33d5e09f
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2017-04-21 22:30:43 +0200

    Added form and improved on templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Question
 
@@ -13,8 +14,21 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request, question_id):
-    response = "You're looking at the results of question {}"
-    return HttpResponse(response.format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', { 'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question {}".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #Redisplay the question voting form
+        context = {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        }
+        return render(request, 'polls/detail.html', context)
+
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
