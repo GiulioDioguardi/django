e6dcd499efd3c537055985c4c04198211774530f
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2017-05-03 22:05:13 +0200

    Made code more readable

--- mysite/static/hextime/hextime.js
+++ mysite/static/hextime/hextime.js
@@ -4,8 +4,9 @@ function startTime() {
     var m = dec2bin(today.getMinutes());
     var s = dec2bin(today.getSeconds());
     var m_s = zeroFill(m, 6) + zeroFill(s, 6);
+    var m_s_hex = zeroFill(parseInt(m_s, 2).toString(16).toUpperCase(), 3)
     document.getElementById('txt').innerHTML =
-    zeroFill(h, 5) + " : " + zeroFill(parseInt(m_s, 2).toString(16).toUpperCase(), 3);
+    zeroFill(h, 5) + " : " + m_s_hex;
     var t = setTimeout(startTime, 500);
 }
 
