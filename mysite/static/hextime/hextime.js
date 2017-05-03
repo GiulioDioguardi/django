1541cf50d9e7ed050be2f6cdbf8e670331d16260
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2017-05-03 21:55:44 +0200

    Hextime now is as wide as screen and fully functional

--- mysite/static/hextime/hextime.js
+++ mysite/static/hextime/hextime.js
@@ -3,8 +3,9 @@ function startTime() {
     var h = dec2bin(today.getHours());
     var m = dec2bin(today.getMinutes());
     var s = dec2bin(today.getSeconds());
+    var m_s = zeroFill(m, 6) + zeroFill(s, 6);
     document.getElementById('txt').innerHTML =
-    zeroFill(h, 5) + " : " + zeroFill(m, 6) + " : " + zeroFill(s, 6);
+    zeroFill(h, 5) + " : " + parseInt(m_s, 2).toString(16).toUpperCase();
     var t = setTimeout(startTime, 500);
 }
 
