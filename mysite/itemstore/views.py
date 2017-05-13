908267e7cb1abb1e6c1e6de378101105146d5bbc
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2017-05-14 00:12:55 +0200

    Added messages

--- mysite/itemstore/views.py
+++ mysite/itemstore/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
 
 from .models import Product
 
@@ -16,10 +17,12 @@ def buy(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     product.stock -= 1
     product.save()
-    return HttpResponseRedirect(reverse('itemstore:index'))
+    messages.success(request, "You have bought %s" % product.name)
+    return redirect('itemstore:index')
 
 def restock(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     product.stock += 1
     product.save()
-    return HttpResponseRedirect(reverse('itemstore:index'))
+    messages.success(request, "You have restocked %s" % product.name)
+    return redirect('itemstore:index')
