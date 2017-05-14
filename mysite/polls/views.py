351a62004ed9b03db96cc8682040e3a0e2461b0f
DjangoServer <django.server@giulio.com>
2017-05-14 13:53:55 +0000

    Fixed race condtions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.db import transaction
 
 from .models import Question
 from .models import Choice
@@ -32,17 +33,18 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        #Redisplay the question voting form
-        context = {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        }
-        return render(request, 'polls/detail.html', context)
+    with transaction.atomic():
+        question = get_object_or_404(Question.objects.select_for_update(), pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            #Redisplay the question voting form
+            context = {
+                'question': question,
+                'error_message': "You didn't select a choice.",
+            }
+            return render(request, 'polls/detail.html', context)
 
-    selected_choice.votes += 1
-    selected_choice.save()
-    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
